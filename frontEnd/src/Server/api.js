d46081565299172f057b3654794625606e9de639
Pradeep <gosaike@outlook.com>
2021-07-27 09:35:30 +0530

    change frontEnd-package.json api.js && package.json server.json

--- frontEnd/src/Server/api.js
+++ frontEnd/src/Server/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = '';
+const URL = 'http://localhost:5000/api';
 
 export const getUser = async (info) => {
     console.log(info);
--- server.js
+++ server.js
@@ -2,18 +2,25 @@ import express from 'express';
 import mongoose from 'mongoose';
 import router from './Router/router.js';
 import cors from 'cors'
+import path from 'path';
 const app = express();
 
 app.use(express.json({ extended : true }));
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
-app.use('/pradeep', router);
+app.use('/api', router);
 
 const PORT = process.env.PORT || 5000;
 const URL = "mongodb+srv://pradeep:passuio@cluster0.i2yya.mongodb.net/STACK?retryWrites=true&w=majority";
 
+__dirname = path.resolve();
+
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('frontEnd/build'));
+    app.use(express.static(path.join(__dirname,'frontEnd/build')));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontEnd", "build", "index.html"));
+    });
 }
 
 mongoose.connect(process.env.MONGODB_URI || URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false }).then(() => {
